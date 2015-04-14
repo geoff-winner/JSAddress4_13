f5e25e5cdc2c0e25441762e29e5d689b0f7f3c50
John Franti & Geoff Winner <student@epicodus.com>
2015-04-14 09:58:33 -0700

    Add address constructorcd

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,18 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val();
+    var inputtedAddress =  { street: inputtedStreet, city: inputtedCity, state: inputtedState, zip: inputtedZip };
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: (inputtedAddress.street + ", " + inputtedAddress.city + ", " + inputtedAddress.state + " " + inputtedAddress.zip) };
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
     $(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+  $("#show-contact").toggle();
+  $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName + " ");
   $(".first-name").text(newContact.firstName);
   $(".last-name").text(newContact.lastName);
   $(".address").text(newContact.address);
